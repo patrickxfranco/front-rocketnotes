b48ee2985d35fee66da916705efd59cf44b2764b
Patrick Franco <patrickxfranco@gmail.com>
2024-09-26 11:42:37 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Routes } from "./routes";
+import { ThemeProvider } from "styled-components";
+
+import { AuthProvider } from "./hooks/auth";
+
+import GlobalStyles from "./styles/global";
+import theme from "./styles/theme";
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+		</ThemeProvider>
+	</StrictMode>
+);
